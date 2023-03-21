5d951090dcbf847256d3c912377264e285b04549
Over <franciscosebastianyjunior@hotmail.com>
2023-03-21 19:58:24 -0300

    first commit

---
+++ linked_list.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+Node *new_node(int data) {
+    Node *node = (Node*) malloc(sizeof(Node));
+    node->value = data;
+    node->next = NULL;
+    node->prev = NULL;
+
+    return node;
+}
+
+Node *new_head(void) {
+    return new_node(0);
+}
+
+void push_node(Node *head, int value) {
+    Node *node = new_node(value);
+
+    if (head->next == NULL) {
+        node->prev = head;
+        head->next = node;
+    } else {
+        push_node(head->next, value);
+    }
+}
+
+void pop_node(Node *head) {
+    if (head->next == NULL) {
+        if (head->prev == NULL)
+            return;
+
+        head->prev->next = NULL;
+        free(head);
+    } else {
+        pop_node(head->next);
+    }
+}
+
+void display_list(Node *head) {
+    if (head->next == NULL)
+        return;
+
+    Node *node = head->next;
+
+    while (node != NULL) {
+        printf("%i ", node->value);
+        node = node->next;
+    }
+
+    printf("\n");
+}
---
+++ linked_list.h
@@ -0,0 +1,10 @@
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Node;
+
+Node *new_node(int data);
+Node *new_head(void);
+void push_node(Node *head, int value);
+void pop_node(Node *head);
+void display_list(Node *head);
